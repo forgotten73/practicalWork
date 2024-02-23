9b7ab3f17ef827001ab57547dd9bff74b3fbb52f
Sergey Tsirkun <chester14011992@gmail.com>
2024-02-23 13:53:50 +0400

    add pom file

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,2 +1,36 @@
-package tests;public class BaseTest {
+package tests;
+
+import helpers.ConfProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.FormPage;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    public static FormPage formPage;
+
+    @BeforeAll
+    public static void setup() {
+        int pageTimeout = Integer.parseInt(ConfProperties.getProperty("pageTimeout"));
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        formPage = new FormPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(pageTimeout));
+        driver.get(ConfProperties.getProperty("url"));
+    }
+
+    @AfterAll
+    public static void testQuit() {
+        driver.quit();
+    }
+
 }
